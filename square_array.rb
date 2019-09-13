847bb9656a9535c9bd3192d5d5f0975ad1a07818
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-13 18:21:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while counter < array.length do
+    number = array[counter]
+    array[counter] = number*number
+    
+    puts array[counter]
+
+    counter += 1
+  end
+  
+  return array
 end
