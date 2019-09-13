cd48c19fec4f9115de0dae0374d6545c3f10d09a
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-13 18:35:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,4 @@
 def square_array(array)
-  counter = 0
-
-  #while counter < array.length do
-    number = array[counter]
-    array[counter] = number*number
-    
-    puts array[counter]
-
-    counter += 1
-  end
-  
-  return array
+  array.each
 end
 
